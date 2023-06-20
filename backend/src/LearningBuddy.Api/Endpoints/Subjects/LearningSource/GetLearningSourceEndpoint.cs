41b1711394d194b40fe1a63c2b27aa7748a147ac
KacperHolubowicz <kac.perhol2000@gmail.com>
2023-06-20 13:21:00 +0200

    Added endpoint for retrieving one learning source

---
+++ backend/src/LearningBuddy.Api/Endpoints/Subjects/LearningSource/GetLearningSourceEndpoint.cs
@@ -0,0 +1,22 @@
+﻿using LearningBuddy.Application.Subjects.Queries.GetLearningSource;
+using LearningBuddy.Application.Subjects.Queries.GetListOfLearningSources;
+
+namespace LearningBuddy.Api.Endpoints.Subjects.LearningSource
+{
+    public class GetLearningSourceEndpoint : BaseEndpoint<GetLearningSourceQuery, LearningSourceDTO>
+    {
+        public override void Configure()
+        {
+            Get(Url + "learning-source/{LearningSourceID}");
+            AllowAnonymous();
+            ResponseCache(60);
+        }
+
+        public override async Task HandleAsync(GetLearningSourceQuery req, CancellationToken ct)
+        {
+            int userId = GetUserFromAuth();
+            req.UserID = userId;
+            await SendAsync(await Mediator.Send(req, ct));
+        }
+    }
+}
